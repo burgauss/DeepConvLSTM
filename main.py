b836128f2543a1ee3d5c97ecf8165cac00387c2a
Juan Burgos <burgos.135@gmail.com>
2022-12-15 13:39:24 +0100

    implementing main_regression

--- main.py
+++ main.py
@@ -77,6 +77,41 @@ def main():
         mkdir_if_missing(log_dir)
         logXtrainYtrain(log_dir, X_train, y_train)  	#Log one sample for thesis purposes
 
+def main_regression():
+    log_date = time.strftime('%Y%m%d')
+    log_timestamp = time.strftime('%H%M%S')
+
+
+    dataLoader = myDataLoader(pathAll_regression, True)
+    dataset_pd, indexes_LS1ON, indexes_LS2ON = dataLoader.processData()
+    X_train, X_valid, y_train, y_valid = getWindowedSplitData(dataset_pd, indexes_LS1ON, indexes_LS2ON, 
+                            tStepLeftShift=0, tStepRightShift=45, testSizePerc=0.15)
+    X_train_ss, X_valid_ss, x_mm = MinMaxNormalization(X_train, X_valid)             # Rescaling
+    y_train_ss, y_valid_ss, y_mm = MinMaxNormalization(y_train, y_valid)
+ 
+    print("X_train shape: ", X_train_ss.shape, "X_test_shape", X_valid_ss.shape)
+    print("y_train shape: ", y_train_ss.shape, "y_test_shape", y_valid_ss.shape)
+    #Adding an extradimension for Pytorch
+    X_train_ss = X_train_ss.reshape((X_train_ss.shape[0], X_train_ss.shape[1], 1))
+    X_valid_ss = X_valid_ss.reshape((X_valid_ss.shape[0], X_valid_ss.shape[1], 1))
+    
+    print("X_train new shape: ", X_train_ss.shape, "y_train shape", y_train.shape)
+    # Converting data for GPU compatibality
+    X_train_ss, y_train = X_train_ss.astype(np.float32), y_train.astype(np.uint8)
+    X_valid_ss, y_valid = X_valid_ss.astype(np.float32), y_valid.astype(np.uint8)
+
+    #Adding two new parameters according to the shape of the datasets
+    config['window_size'] = X_train_ss.shape[1]
+    config['nb_channels'] = X_train_ss.shape[2]
+
+    if config['valid_type'] == "split":
+        net = DeepConvLSTM_regression(config=config)
+        loss = torch.nn.MSELoss()
+        opt = torch.optim.Adam(net.parameters(), lr=config['lr'], weight_decay=config["weight_decay"])
+        trained_net = train_valid_split(x_train_set = X_train_ss, y_train_set = y_train_ss,
+             x_valid_set = X_valid_ss, y_valid_set = y_valid_ss, custom_net=net, custom_loss=loss, custom_opt=opt)
+        torch.save(trained_net.state_dict(), './model'+ log_date + log_timestamp +'.pth')
+
 if __name__ == "__main__":
     if config['DL_mode'] == 'classification':
         main()
